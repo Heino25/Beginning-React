f5403e9a166de2c937dcd5edea3f6f59655132f2
unknown <heinoduplessis@gmail.com>
2023-01-23 12:45:24 +0200

    01-23-23

--- Beginnig React/project_name/src/App.js
+++ Beginnig React/project_name/src/App.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
 import Products from './Products';
-import { Button } from 'react-bootstrap';
-import Rating from './Rating';
 
 class App extends Component {
-    render() {
-        const isValid = true;
-
-        return (
-            <div>
-                <h1>
-                    My First React App, Lol!
-                    <Products />
-                    <Button variant="primary" disabled={!isValid}>Default</Button>
-
-                </h1>
-                <Rating rating="1"/>
-                <Rating rating="2"/>
-                <Rating rating="3"/>
-                <Rating rating="4"/>
-                <Rating rating="5"/>
-            </div>
-        );
-    }
+  render() {        
+    return (
+      <div>
+        <Products />        
+      </div>
+    );
+  }
 }
 
 export default App;
