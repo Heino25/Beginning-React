c286d4939070af162e9b98b04c2b07586c8c5d4e
unknown <heinoduplessis@gmail.com>
2023-01-26 18:09:15 +0200

    26-01-23

--- Beginnig React/chapter_7_getting_data/src/App.js
+++ Beginnig React/chapter_7_getting_data/src/App.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
 import GitHub from './GitHub';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { render } from '@testing-library/react';
 
 class App extends Component { 
       
   render() {        
     return (
       <div>
-        <h1>Hello</h1>
-        <GitHub />                        
+        <Header />                        
       </div>
     );
   }
 }
 
 export default App;
+
+class Header extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/github" component={GitHub} />
+          <Route exact path="/" component={Home} />
+          <Route path="/*" component={NotFound} />
+        </Switch>
+      </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+class Home extends Component {
+  render(){
+    return (
+      <div>
+        Home
+      </div>
+    )
+  }
+}
+
+class NotFound extends Component {
+  render(){
+    return <div>Not Found</div>
+  }
+}
