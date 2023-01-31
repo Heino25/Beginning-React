58bff0b671cd6f570c9eac0cdea4a6654a3bd62a
unknown <heinoduplessis@gmail.com>
2023-01-31 13:13:17 +0200

    '31-01-23'

--- Beginnig React/react-18-shopping-cart/src/components/Header.js
+++ Beginnig React/react-18-shopping-cart/src/components/Header.js
@@ -1,14 +1,13 @@
 export default function Header(){
     return (
-        <div className="block">
+        <div className="row center block">
             <div>
                 <a href="#/">
                     <h2>Simple Shopping Cart</h2>
                 </a>
             </div>
             <div>
-                <a href="#cart">Cart</a>
-                <a href="#cart">Sign In</a>
+                <a href="#cart">Cart</a> <a href="#cart">Sign In</a>
             </div>
         </div>
     )
--- Beginnig React/react-18-shopping-cart/src/components/Main.js
+++ Beginnig React/react-18-shopping-cart/src/components/Main.js
@@ -1,3 +1,11 @@
-export default function Main(){
-    return <div className="col-2">Main</div>;
+export default function Main(props){
+    const { products } = props;
+    return <div className=" block col-2">
+        <h2>Products</h2>
+        <div className="row">
+            {products.map((product) => (
+                <div key={product.id}>{product.name}</div>
+            ))}
+        </div>
+    </div>;
 }
