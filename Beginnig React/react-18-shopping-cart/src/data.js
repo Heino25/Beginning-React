355fe6153e2c0caf605ae2bc6ac940b564c737ee
unknown <heinoduplessis@gmail.com>
2023-02-01 16:24:38 +0200

    '01-02-23'

--- Beginnig React/react-18-shopping-cart/src/data.js
+++ Beginnig React/react-18-shopping-cart/src/data.js
@@ -4,7 +4,7 @@ const data = {
             id: '1',
             name: 'Macbook',
             price: 1400,
-            image: '',
+            image: 'https://picsum.photos/id/180/2400.1600',
         },
         {
             id: '2',
