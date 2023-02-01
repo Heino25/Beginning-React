87deb26b6ce30c5bf3cc4eb9e7daed7148548b92
unknown <heinoduplessis@gmail.com>
2023-02-01 16:23:42 +0200

    '01-02-23'

--- Beginnig React/react-18-shopping-cart/src/components/Header.js
+++ Beginnig React/react-18-shopping-cart/src/components/Header.js
@@ -1,14 +1,24 @@
-export default function Header(){
-    return (
-        <div className="row center block">
-            <div>
-                <a href="#/">
-                    <h2>Simple Shopping Cart</h2>
-                </a>
-            </div>
-            <div>
-                <a href="#cart">Cart</a> <a href="#cart">Sign In</a>
-            </div>
-        </div>
-    )
+import React from 'react';
+
+export default function Header(props) {
+  return (
+    <header className="block row center">
+      <div>
+        <a href="#/">
+          <h1>Small Shopping Cart</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">
+          Cart{' '}
+          {props.countCartItems ? (
+            <button className="badge">{props.countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
+        <a href="#/signin"> SignIn</a>
+      </div>
+    </header>
+  );
 }
--- Beginnig React/react-18-shopping-cart/src/components/Main.js
+++ Beginnig React/react-18-shopping-cart/src/components/Main.js
@@ -1,11 +1,16 @@
-export default function Main(props){
-    const { products } = props;
-    return <div className=" block col-2">
-        <h2>Products</h2>
-        <div className="row">
-            {products.map((product) => (
-                <div key={product.id}>{product.name}</div>
-            ))}
-        </div>
-    </div>;
+import React from 'react';
+import Product from './Product';
+
+export default function Main(props) {
+  const { products, onAdd } = props;
+  return (
+    <main className="block col-2">
+      <h2>Products</h2>
+      <div className="row">
+        {products.map((product) => (
+          <Product key={product.id} product={product} onAdd={onAdd}></Product>
+        ))}
+      </div>
+    </main>
+  );
 }
